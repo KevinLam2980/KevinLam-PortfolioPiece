1d8f2fb493b382d588f131ad586cda279334e87c
Kevin Lam <61953680+KevinLam2980@users.noreply.github.com>
2020-12-02 14:08:57 -0800

    add button for git repositories for projects

--- src/components/header.js
+++ src/components/header.js
@@ -3,22 +3,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 const Header = props => {
-    
-    // document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    //     anchor.addEventListener('click', function(e) {
-    //         e.preventDefault();
-    //         document.querySelector(this.getAttribute('href')).scrollIntoView({
-    //             behavior: 'smooth'
-    //         });
-    //     });
-    // });
 
     return (
         <section id="intro">
             <h1>Kevin Lam</h1>
             <h2>Web Developer</h2>
             <p>Full Stack developer specializing in Javascript, React, Redux, and Java Spring</p>
-                {/* <a href="#nav" className="button icon circle actions" id="scrollerBTN">scroll</a> */}
             <div id="socialMobile">
                 <a href="https://github.com/KevinLam2980" alt="Github link" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={["fab", "github"]}/></a>
                 <a href="https://www.linkedin.com/in/kevin-lam-8729281a3/" alt="Linkedin link" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={["fab", "linkedin"]}/></a>
