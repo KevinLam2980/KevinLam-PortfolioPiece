145e2e81c7411b794ef46e64fab9b103016e52da
Kevin Lam <61953680+KevinLam2980@users.noreply.github.com>
2020-12-09 18:58:09 -0800

    fixed styling for project so that buttons are more align

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,25 +7,34 @@ const Project = props => {
 
 
     return (
-        <article>    
-        <FadeInSection>
-            <header>
-                <span className="date">{city.date}</span>
-                <h2>{city.title}</h2>
-            </header>
-            <img className="image-fit" src={require(`../assets/images/${city.img}.png`)} alt="Project screenshot" />
+        <article className="project">
+            <div className="projectHeader">
+                <FadeInSection>
+                    <header>
+                        <span className="date">{city.date}</span>
+                        <h2>{city.title}</h2>
+                    </header>
+                    <img className="image-fit" src={require(`../assets/images/${city.img}.png`)} alt="Project screenshot" /> 
             </FadeInSection>
-            <FadeInSection>
-            <p>{city.description}</p>
-            </FadeInSection>
-            <FadeInSection>
-            {
-            city.projectLink !== "" ? <a href={city.projectLink} target="_blank"  className="button" rel="noopener noreferrer">View Project</a> : null
-            }
-            {
-            city.githubLink !== "" ? <a href={city.githubLink} alt="Github repository link" className="button githubButton" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="githubIcon" icon={["fab", "github"]}/></a> : null
-            }
-          </FadeInSection>
+            </div>
+            <div className="projectDescription">
+                <FadeInSection>
+                    <p>{city.description}</p>
+                </FadeInSection>
+            </div>
+            <div className="projectButtons">
+                <FadeInSection >
+                    {
+                    city.projectLink !== "" ? <a href={city.projectLink} target="_blank"  className="button" rel="noopener noreferrer">View Project</a> : null
+                    }
+                    {
+                    city.githubLink !== "" ? <a href={city.githubLink} alt="Github repository link" className="button githubButton" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="githubIcon" icon={["fab", "github"]}/></a> : null
+                    }     
+                </FadeInSection>
+            </div>
+    
+    
+            
     </article> 
     )
 }
