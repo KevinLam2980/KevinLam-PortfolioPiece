3334e69aad2e843b1c9a213a354ebe20913eedd3
Kevin Lam <61953680+KevinLam2980@users.noreply.github.com>
2021-01-29 20:10:45 -0800

    compressed images for performace and quicker rendering on component mount

--- src/assets/data/projectsRepo.js
+++ src/assets/data/projectsRepo.js
@@ -2,7 +2,7 @@ const listOfProjects = [
     {
         date: "September 2020",
         title: "Citrics",
-        img: 'citrics',
+        img: 'citrics-min',
         description: "Full stack informational website used to compare different cities within the United States. Users can view data stats of cities such as; population, weather, cost of rent, and cost of homes",
         // projectLink: "https://main.d2rhibc2z6ms0a.amplifyapp.com",
         projectLink: "https://citycitrics.netlify.app/",
@@ -11,7 +11,7 @@ const listOfProjects = [
     {
         date: "July 24, 2020",
         title: "Secret Family Recipes",
-        img: 'secret-recipe',
+        img: 'secret-recipe-min',
         description: "React application with full CRUD capabilities, allowing users to add, edit, and delete their own cooking recipes.",
         projectLink: "https://secret-family-recipes-app.vercel.app",
         githubLink: "https://github.com/KevinLam2980/secret-family-recipes"
@@ -19,7 +19,7 @@ const listOfProjects = [
     {
         date: "June 26, 2020",
         title: "Spotify Suggester",
-        img: 'SpotifySuggester2',
+        img: 'SpotifySuggester2-min',
         description: "A song suggester application built to give users recommended songs based on the accousticness, danceability, energy, instrumentalness, liveness, loudness, speechiness, tempo, and valence if your favorite songs.",
         // projectLink: "https://spotify-suggester-melon.netlify.app/landing.html",
         projectLink: "https://spotify-suggester-rho.vercel.app",
@@ -28,7 +28,7 @@ const listOfProjects = [
     {
         date: "Jan 7, 2021",
         title: "Movie Nominator",
-        img: 'shoppies',
+        img: 'shoppies-min',
         description: "A front end react application that allows users to search up and select 5 of their favorite movies to nominate them for best movie of the year. Users can also look at movie information such as the plot, actors, writers, and more.",
         projectLink: "https://shoppies-movie-app.vercel.app",
         githubLink: "https://github.com/KevinLam2980/Shoppies"
