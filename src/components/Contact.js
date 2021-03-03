8de85014e2c7d8d11394b75c045b1fc103d7acd8
Kevin Lam <kev2980@gmail.com>
2021-03-03 14:58:05 -0800

    update message styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -51,14 +51,14 @@ const Contact = (props) => {
     'header',
     'bold', 'italic', 'underline', 'strike', 'blockquote',
     'list', 'bullet', 'indent',
-    'link', 'image'
+    'link'
   ]
   const modules = {
     toolbar: [
       [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
       ['bold', 'italic', 'underline','strike', 'blockquote'],
       [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-      ['link', 'image'],
+      ['link'],
       ['clean']
     ],
   }
@@ -197,6 +197,7 @@ const Contact = (props) => {
                 onChange={contentChangeHandle}
                 modules={modules}
                 formats={formats} 
+                placeholder="Type your message here."
               />
               <textarea 
                 style={{ display: 'none' }}
