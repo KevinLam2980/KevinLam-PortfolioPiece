f98c4eec99d0d63f36b70cfe47b93d2079466fef
Kevin Lam <kev2980@gmail.com>
2021-03-03 14:47:01 -0800

    add rich text editor to messaging input

--- src/Validation/newContact.js
+++ src/Validation/newContact.js
@@ -8,7 +8,7 @@ const formSchema = yup.object().shape({
         .string().email("Email must be in proper email format: 'example@email.com'"),
     message: yup
         .string()
-        .min(10, "Message must be atleast 10 characters")
+        .min(15, "Message must be atleast 15 characters")
 })
 
 export default formSchema
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import emailjs from 'emailjs-com'
-import Styled from 'styled-components'
-import * as yup from 'yup'
-import newContact from '../Validation/newContact'
-import { faTruckMonster } from '@fortawesome/free-solid-svg-icons'
+import React, { useState, useEffect, useRef } from "react";
+import emailjs from "emailjs-com";
+import Styled from "styled-components";
+import * as yup from "yup";
+import newContact from "../Validation/newContact";
+import ReactQuill from "react-quill";
 
 const SuccessMessage = Styled.p`
     /* transition: 0.4s ease-in-out;
@@ -18,7 +18,7 @@ const SuccessMessage = Styled.p`
     position: absolute;
     top: 0;
     box-shadow: 0px 1px 10px black;
-`
+`;
 
 const FailMessage = Styled.p`
     transition: 0.4s ease-in-out;
@@ -32,143 +32,216 @@ const FailMessage = Styled.p`
     position: absolute;
     top: 0;
     box-shadow: 0px 1px 10px black;
-`
+`;
 
 let initialFormValues = {
-    name: "",
-    email: "",
-    message: ""
-}
+  name: "",
+  email: "",
+  message: "",
+};
 
-const Contact = props => {
-    const [sent, setSent] = useState(false)
-    const [emailError, setEmailError] = useState(false)
-    const [emailFormValues, setEmailFormValues] = useState(initialFormValues)
-    const [emailFormErrors, setEmailFormErrors] = useState(initialFormValues)
-    const [formDisabled, setFormDisabled] = useState(true)
+const Contact = (props) => {
+  const [sent, setSent] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [emailFormValues, setEmailFormValues] = useState(initialFormValues);
+  const [emailFormErrors, setEmailFormErrors] = useState(initialFormValues);
+  const [formDisabled, setFormDisabled] = useState(true);
 
-    function onHandleChange(e){
-        const {name, value} = e.target
-        yup
-        .reach(newContact, name)
-        .validate(value)
-        .then(valid => {
-            setEmailFormErrors({
-                ...setEmailFormErrors,
-                [name]: "",
-            })
-        })
-        .catch(err => {
-            setEmailFormErrors({
-                ...emailFormErrors,
-                [name]: err.errors[0],
-            })
-        })
-        setEmailFormValues({
-            ...emailFormValues, 
-            [name]: value
-        })
+  const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+  ]
+  const modules = {
+    toolbar: [
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      ['bold', 'italic', 'underline','strike', 'blockquote'],
+      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+      ['link', 'image'],
+      ['clean']
+    ],
+  }
+
+  function onHandleChange(e) {
+    const { name, value } = e.target;
+    yup
+      .reach(newContact, name)
+      .validate(value)
+      .then((valid) => {
+        setEmailFormErrors({
+          ...setEmailFormErrors,
+          [name]: "",
+        });
+      })
+      .catch((err) => {
+        setEmailFormErrors({
+          ...emailFormErrors,
+          [name]: err.errors[0],
+        });
+      });
+    setEmailFormValues({
+      ...emailFormValues,
+      [name]: value,
+    });
+  }
+
+    function contentChangeHandle(content) {
+      yup
+      .reach(newContact, "message")
+      .validate(content.replace(/<[^>]*>/g, ''))
+      .then((valid) => {
+        setEmailFormErrors({
+          ...emailFormErrors,
+          message: "",
+        });
+      })
+      .catch((err) => {
+        setEmailFormErrors({
+          ...emailFormErrors,
+          message: err.errors[0],
+        });
+      });
+    setEmailFormValues({
+      ...emailFormValues,
+      message: content.toString(),
+    });
     }
 
-    useEffect(() => {
-        newContact.isValid(emailFormValues).then(valid => {
-            setFormDisabled(!valid)
-        })
-    })
+  useEffect(() => {
+    newContact.isValid(emailFormValues).then((valid) => {
+      setFormDisabled(!valid);
+    });
+  });
 
-    function sendEmail(e){
-        e.preventDefault()
+  function sendEmail(e) {
+    e.preventDefault();
 
-        emailjs.sendForm('service_gxzvmu7', 'template_xbbrz01', e.target, 'user_rey8jOSF8bjlw9NSzdz4m')
-        .then((res) => {
-            console.log(res.text)
-            setSent(true)
-            setTimeout(() => {
-                setSent(false)
-                setEmailFormValues(initialFormValues)
-            }, 6000)
-        }, (err) => {
-            console.log(err.text)
-            setEmailError(true)
-            setTimeout(() => {
-                setEmailError(false)
-            }, 5000)
-        })
-    }
+    emailjs
+      .sendForm(
+        "service_gxzvmu7",
+        "template_xbbrz01",
+        e.target,
+        "user_rey8jOSF8bjlw9NSzdz4m"
+      )
+      .then(
+        (res) => {
+          console.log(res.text);
+          setSent(true);
+          setTimeout(() => {
+            setSent(false);
+            setEmailFormValues(initialFormValues);
+          }, 6000);
+        },
+        (err) => {
+          console.log(err.text);
+          setEmailError(true);
+          setTimeout(() => {
+            setEmailError(false);
+          }, 5000);
+        }
+      );
+  }
 
-    return (
-        <div id="contact">
+  return (
+    <div id="contact">
+      <section>
+        {sent ? (
+          <SuccessMessage>
+            Your email was sent! Thank you for reaching out, I will get back to
+            you as soon as I can.
+          </SuccessMessage>
+        ) : null}
+        {emailError ? (
+          <FailMessage>
+            Error! A problem occured while trying to send your email! Please try
+            again.
+          </FailMessage>
+        ) : null}
+        <form onSubmit={sendEmail}>
+          <div class="fields">
+            <div class="field">
+              <label htmlFor="name">Name</label>
+              <div id="nameError" className="validationMessage">
+                {emailFormErrors.name}
+              </div>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                onChange={onHandleChange}
+                value={emailFormValues.name}
+              />
+            </div>
+            <div class="field">
+              <label htmlFor="email">Email</label>
+              <div id="emailError" className="validationMessage">
+                {emailFormErrors.email}
+              </div>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                onChange={onHandleChange}
+                value={emailFormValues.email}
+              />
+            </div>
+            <div class="field">
+              <label htmlFor="message">Message</label>
+              <div id="messageError" className="validationMessage">
+                {emailFormErrors.message}
+              </div>
+              <ReactQuill
+                theme="snow"
+                value={emailFormValues.message}
+                onChange={contentChangeHandle}
+                modules={modules}
+                formats={formats} 
+              />
+              <textarea 
+                style={{ display: 'none' }}
+                name="message"
+                id="message" 
+                value={emailFormValues.message}
+                ></textarea>
+            </div>
+          </div>
+          <input
+            className="button"
+            type="submit"
+            value="Send"
+            disabled={formDisabled}
+          />
+        </form>
+      </section>
+
+      <section class="split contact">
         <section>
-            {sent ? <SuccessMessage>Your email was sent! Thank you for reaching out, I will get back to you as soon as I can.</SuccessMessage> : null}
-            {emailError ? <FailMessage>Error! A problem occured while trying to send your email! Please try again.</FailMessage> : null}
-            <form onSubmit={sendEmail}>
-                <div class="fields">
-                    <div class="field">
-                        <label htmlFor="name">Name</label>
-                        <div id="nameError" className="validationMessage">
-                            {emailFormErrors.name}
-                        </div>
-                        <input 
-                        type="text" 
-                        name="name" 
-                        id="name" 
-                        onChange={onHandleChange} 
-                        value={emailFormValues.name}/>
-                    </div>
-                    <div class="field">
-                        <label htmlFor="email">Email</label>
-                        <div id="emailError" className="validationMessage">
-                            {emailFormErrors.email}
-                        </div>
-                        <input 
-                        type="email" 
-                        name="email" 
-                        id="email" 
-                        onChange={onHandleChange} 
-                        value={emailFormValues.email}/>
-                    </div>
-                    <div class="field">
-                        <label htmlFor="message">Message</label>
-                        <div id="messageError" className="validationMessage">
-                            {emailFormErrors.message}
-                        </div>
-                        <textarea 
-                        name="message" 
-                        id="message" 
-                        rows="5"
-                        onChange={onHandleChange} 
-                        value={emailFormValues.message}
-                        ></textarea>
-                    </div>
-                </div>
-                    <input 
-                    className="button"
-                    type="submit" 
-                    value="Send"
-                    disabled={formDisabled}
-                     />
-            </form>
+          <h3>Phone</h3>
+          <p className="contactInfo">(408) 532-3643</p>
         </section>
-
-        <section class="split contact">
-            <section>
-                <h3>Phone</h3>
-                <p className="contactInfo">(408) 532-3643</p>
-            </section>
-            <section>
-                <h3>Email</h3>
-                <p className="contactInfo">kev2980@gmail.com</p>
-            </section>
-            <section>
-                <h3>Social</h3>
-                <ul class="icons alt">
-                    <li><a href="https://www.linkedin.com/in/kevin-lam-8729281a3/"><span class="label">Linkedin</span></a></li>
-                    <li><a href="https://github.com/KevinLam2980"><span class="label">GitHub</span></a></li>
-                </ul>
-            </section>
+        <section>
+          <h3>Email</h3>
+          <p className="contactInfo">kev2980@gmail.com</p>
         </section>
-    </div> 
-    )
-}
+        <section>
+          <h3>Social</h3>
+          <ul class="icons alt">
+            <li>
+              <a href="https://www.linkedin.com/in/kevin-lam-8729281a3/">
+                <span class="label">Linkedin</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/KevinLam2980">
+                <span class="label">GitHub</span>
+              </a>
+            </li>
+          </ul>
+        </section>
+      </section>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
