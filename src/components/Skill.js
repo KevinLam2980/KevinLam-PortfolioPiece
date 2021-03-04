d004b4797f7271de0759a93168d3890de6225f23
Kevin Lam <kev2980@gmail.com>
2021-03-03 17:21:53 -0800

    change layout for about component, made icons smaller, fixed wrapping for mobile

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import Styled from 'styled-components'
+import React from "react";
+import Styled from "styled-components";
 
 const SkillDiv = Styled.div`
 display: inline-block;
@@ -9,34 +9,53 @@ display: inline-block;
     color: #212931;
 }
 .featuredSkillIcon {
-width: 100px;
-height: 100px;
+width: 50px;
+height: 50px;
 margin: 1.5rem;
 @media (max-width: 520px) { 
-    width: 75px !important;
-	height: 75px !important;
+    width: 40px !important;
+	height: 40px !important;
 }
 }
 &:hover {
     transform: scale(1.1);
     transition: 0.75s;
 }
-`
+`;
 
-const Skill = props => {
-    const {skill} = props
+const Skill = (props) => {
+  const { skill } = props;
 
-    return (
-        <SkillDiv>
-            {
-            skill.imgType === "png" ? <img className="featuredSkillIcon" src={require(`../assets/images/SkillsImages/${skill.img}.png`)} alt={skill.alt}/> : 
-            skill.imgType === "svg" ? <img className="featuredSkillIcon" src={require(`../assets/images/SkillsImages/${skill.img}.svg`)} alt={skill.alt}/> : 
-            skill.imgType === "jpeg" ? <img className="featuredSkillIcon" src={require(`../assets/images/SkillsImages/${skill.img}.jpeg`)} alt={skill.alt}/> :
-            skill.imgType === "webp" ? <img className="featuredSkillIcon" src={require(`../assets/images/SkillsImages/${skill.img}.webp`)} alt={skill.alt}/> : null
-            }
-            <p className="skillLabel">{skill.label}</p>
-        </SkillDiv>
-    )
-}
+  return (
+    <SkillDiv>
+      {skill.imgType === "png" ? (
+        <img
+          className="featuredSkillIcon"
+          src={require(`../assets/images/SkillsImages/${skill.img}.png`)}
+          alt={skill.alt}
+        />
+      ) : skill.imgType === "svg" ? (
+        <img
+          className="featuredSkillIcon"
+          src={require(`../assets/images/SkillsImages/${skill.img}.svg`)}
+          alt={skill.alt}
+        />
+      ) : skill.imgType === "jpeg" ? (
+        <img
+          className="featuredSkillIcon"
+          src={require(`../assets/images/SkillsImages/${skill.img}.jpeg`)}
+          alt={skill.alt}
+        />
+      ) : skill.imgType === "webp" ? (
+        <img
+          className="featuredSkillIcon"
+          src={require(`../assets/images/SkillsImages/${skill.img}.webp`)}
+          alt={skill.alt}
+        />
+      ) : null}
+      <p className="skillLabel">{skill.label}</p>
+    </SkillDiv>
+  );
+};
 
-export default Skill
+export default Skill;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -11,26 +11,33 @@ const SkillsDiv = Styled.div`
     margin-top: 2rem;
 }
 .featuredSkillIcon {
-width: 100px;
-height: 100px;
+width: 50px;
+height: 50px;
 margin: 1.5rem;
 &:hover {
     transform: scale(1.25);
     transition: 0.75s;
 }
 }
+.skillsHeaders {
+    width: 100%;
+}
+.featuredSkillsContainer {
+    width: 100%;
+}
 `
 
 const Skills = () => {
 
     return (
-        <SkillsDiv id="main">
+        <SkillsDiv id="main" className="aboutMeComponent">
             <article className="post">
                 <header className="major aboutMe">
                     <h2 className="aboutMeHeader">About me</h2>
                     <FadeInSection>
                         <div className="aboutSection">
-                          <p>Welcome! My name is Kevin, I am a full stack web developer. I love building and desigining the look and functionality of web applications and will never get over the fact that such amazing things can be created with just a laptop and some will power. When I'm not coding applications, I'm usually keeping up with tech trends, video editing, listening to music, or playing games. If you'd like to find out more about what I've been doing, please don't hesitate to contact me, as I will be looking forward to hearing from you.</p>
+                          <p><strong id="aboutIntro">Hello!</strong> My name is Kevin, I am a passionate full stack developer who can get carried away with time while thinking about the little details of my projects. I have a track record of building applications using modern frameworks, I love building and desigining the look and functionality of challenging projects, and I get immense satisfaction out of knowing that there are people in this world who have used and experienced something that I created. I pride myself over the idea of being a forever student who is always working to hone my skills, better myself, and progress my career. When I'm not developing, playing around with new technologies, or keeping up with the latest development trends and best practices, I'm usually thinking about astrophysics, video editing, listening to music, mining cripto, or playing games during my spare time.</p> 
+                          <p> If you'd like to find out more about what I've been working on, or you have an interesting project idea that you might want to talk to a developer about, please don't hesitate to contact me. I look forward to hearing from you!</p>
                         </div>
                     </FadeInSection>
                 </header>
@@ -67,7 +74,7 @@ const Skills = () => {
                 <header className="major">
                     <h2 className="skillsHeaders">Additional Skills</h2>
                     <FadeInSection>
-                        <div className="additionalSkillsContainer">
+                        <div className="featuredSkillsContainer">
                             {
                                 additionalSkillsRepo.map(skill => {
                                     return <Skill skill={skill} />
